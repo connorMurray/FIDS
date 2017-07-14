cab6f6ab052db72a6c2f714e85c8fe34645ad043
connorMurray <connor.murray@sita.aero>
2017-07-14 20:46:15 +0100

    Adding Angular 4 skeleton project with PrimeNG configured

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { FIDSPage } from './app.po';
+
+describe('fids App', () => {
+  let page: FIDSPage;
+
+  beforeEach(() => {
+    page = new FIDSPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+import {DataListModule, ButtonModule} from 'primeng/primeng';
+
+import {AppComponent} from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    DataListModule,
+    ButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
